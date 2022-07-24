567164997473dabad75443145174760d7168df7a
Yared-betsega <yaredtsegaye120@gmail.com>
2022-07-24 11:44:36 +0300

    Added the user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,3 +1,57 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
+
+const userSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            min: 3, 
+            max: 50
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            min: 5,
+            max: 100
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 4,
+            max: 2048
+        }
+    }
+);
+
+// Generates a token for user
+userSchema.methods.generateToken = function() {
+    const token = jwt.sign(
+        {
+            _id: this.id,
+            email: this.email,
+            username: this.username
+        },
+        process.env["JWT_PRIVATE_KEY"]
+    );
+    return token;
+};
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+    const schema = Joi.object(
+        {
+            username: Joi.string().min(3).max(50).required(),
+            email: Joi.string().min(5).max(100).required().email(),
+            password: Joi.string().min(4).max(2048).required()
+        }
+    );
+
+    return schema.validate(user);
+}
+
+exports.User = User;
+exports.validate = validateUser;
