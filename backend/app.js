6c606a5092c3c95ba99596f98d873c36e2b6942d
Yared-betsega <yaredtsegaye120@gmail.com>
2022-07-24 15:06:31 +0300

    Added the user route/endpoint

---
+++ backend/app.js
@@ -0,0 +1,2 @@
+console.log(process.env["JWT_PRIVATE_KEY"])
+console.log(process.env["port"])
---
+++ backend/routes/users.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { User, validate } = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const router = express.Router();
+
+router.get("/", (req, res) =>
+    User.find({})
+        .then((users) => res.send(users))
+        .catch((error) => res.status(400).send("Cannot get users"))
+);
+
+router.get("/:id", async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).send("User not found");
+    res.send(user);
+});
+router.post("/", async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const emailExists = await User.findOne({ email: req.body.email });
+    const usernameExists = await User.findOne({ username: req.body.username });
+    if (emailExists) return res.status(400).send("Email already exists");
+    if (usernameExists) return res.status(400).send("Username already exists");
+    let user = User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await user.save();
+    const token = user.generateToken();
+    try {
+        res.header("x-auth-token", token).send({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+        });
+    } catch (e) {
+        res.status(400).send("Cannot create user");
+    }
+});
+router.put("/:id", async (req, res) => {
+    let user = await User.findOne({ _id: req.params.id });
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    await user.save();
+    try {
+        res.send(user);
+    } catch (err) {
+        res.status(400).send("Something went wrong!");
+    }
+});
+router.delete("/:id", async (req, res) => {
+    const user = await User.findOne({_id: req.params.id});
+    if (!user) return res.status(400).send("User doesn't exist");
+    try {
+        await User.deleteOne({_id: req.params.id});
+    }catch (e){
+        res.status(400).send("Error while deleting");
+    }
+});
