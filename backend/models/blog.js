17ffc8dfc93bd07f131d684a00ac248f84dadbaa
Yared-betsega <yaredtsegaye120@gmail.com>
2022-07-25 10:51:21 +0300

    added blog model

---
+++ backend/models/blog.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Joi = require("joi")
+
+const blogSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50
+        },
+        author: {
+            type: mongoose.ObjectId,
+            required: true
+        },
+        category: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            required: true,
+            default: Date.now
+        },
+        description: {
+            type: String,
+            max: 200
+        },
+    }
+);
+const Post = mongoose.model("Post", blogSchema);
+
+function validateBlog(blog){
+    const schema = Joi.object(
+        {
+            name: Joi.string().min(2).max(50).required(),
+            author: Joi.string().hex().length(24).required(),
+            category: Joi.string().required(),
+            description: Joi.string().max(200)
+        }
+    )
+    return schema.validate(blog);
+}
+
+module.exports = {
+    Post: Post,
+    validate: validateBlog
+}
